3f5d039f45137c3c7d898c36e3acd8b73da109a8
SYJ <syj3699_@naver.com>
2023-09-01 15:38:02 +0900

    230901

--- script/header.js
+++ script/header.js
@@ -1,8 +1,7 @@
 window.onload = function () {
-
-    //gnb
     const gnbList = document.querySelector('.gnb-list');
     const menuWrap = document.querySelectorAll('.gnb-list > li');
+    const mainMenuWrap = document.querySelectorAll('.main-menu-wrapper');
     const mainMenu = document.querySelectorAll('.main-menu');
     const subMenu = document.querySelectorAll('.sub-menu');
 
@@ -13,17 +12,36 @@ window.onload = function () {
 
     let timer;
 
+    const langBtn = document.querySelector('.lang-box > button');
+    const langList = document.querySelector('.lang-list');
 
+
+    //scroll
+    window.addEventListener('scroll', function () {
+        console.log(window.scrollY);
+        if (window.scrollY > 0) {
+            hContainer.style.backgroundColor = 'white';
+            hBottom.style.borderBottom = 'solid 1px #e7e7e7';
+        }else{
+            hContainer.style.backgroundColor = '';
+            hBottom.style.borderBottom = '';
+        }
+    })
+
+    //gnb
     gnbList.addEventListener('mouseenter', onMenu)
     hContainer.addEventListener('mouseleave', outMenu)
 
-
-    mainMenu.forEach((item) => {
+    mainMenuWrap.forEach((item) => {
         item.addEventListener('mouseenter', function () {
-            item.classList.add('on');
+            mainMenu.forEach((menuItem) => {
+                menuItem.classList.add('on');
+            })
         })
-        item.addEventListener('mouseleave', function(){
-            item.classList.remove('on');
+        item.addEventListener('mouseleave', function () {
+            mainMenu.forEach((menuItem) => {
+                menuItem.classList.remove('on');
+            })
         })
     })
 
@@ -59,14 +77,8 @@ window.onload = function () {
         }, 0)
     }
 
-
-
     //icon
-    const langBtn = document.querySelector('.lang-box > button');
-    const langList = document.querySelector('.lang-list');
-
     langBtn.addEventListener('click', function () {
-        console.log(langBtn)
         if (langList.style.visibility == 'hidden') {
             langList.style.visibility = 'visible';
             langList.style.height = '110px';
@@ -75,9 +87,4 @@ window.onload = function () {
             langList.style.height = '0px'
         }
     })
-
-
-
-
-
 }
