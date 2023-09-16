2de80b33bef3cb463a59c41e5906098a90686e7b
SYJ <syj3699_@naver.com>
2023-09-16 22:34:53 +0900

    230916

--- script/main.js
+++ script/main.js
@@ -1,30 +1,54 @@
-const slider = new Swiper('.swiper', {
+const slider01 = new Swiper('.swiper01', {
     slidesPerView: 6,
     spaceBetween: '18px',
     allowTouchMove: false,
     navigation: {
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next'
+        prevEl: '.btn01-prev',
+        nextEl: '.btn01-next'
     }
 })
 
-const slideItem = document.querySelectorAll('.item');
+const slider02 = new Swiper('.swiper02', {
+    slidesPerView: 6,
+    spaceBetween: '18px',
+    allowTouchMove: false,
+    navigation: {
+        prevEl: '.btn02-prev',
+        nextEl: '.btn02-next'
+    }
+})
+
+
+const slideItem01 = document.querySelectorAll('.item01');
+const slideItem02 = document.querySelectorAll('.item02');
+
 const itemName = document.querySelectorAll('.item-name');
 
-slideItem.forEach((item)=>{
+
+slideItem01.forEach((item)=>{
     item.addEventListener('mouseenter', function(){
         item.classList.remove('on');
         item.classList.add('on');
 
-        itemName.forEach((name)=>{
-            name.innerHTML = 'VIEW MORE';
-        })
+        // itemName.forEach((name)=>{
+        //     name.innerHTML = 'VIEW MORE';
+        // })
     })
     item.addEventListener('mouseleave', function(){
         item.classList.remove('on');
 
-        itemName.forEach((name)=>{
-            name.innerHTML = '';
-        })
+        // itemName.forEach((name)=>{
+        //     name.innerHTML = '';
+        // })
+    })
+})
+
+slideItem02.forEach((item)=>{
+    item.addEventListener('mouseenter', function(){
+        item.classList.remove('on');
+        item.classList.add('on');
+    })
+    item.addEventListener('mouseleave', function(){
+        item.classList.remove('on');
     })
 })
